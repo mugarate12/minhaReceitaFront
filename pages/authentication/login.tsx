14c4a4dd7d48ba5bc3db530419b9b78e014e6d37
mugarate12 <serjumano17@gmail.com>
2020-11-19 16:27:59 -0300

    added material UI inputs on login page

--- pages/authentication/login.tsx
+++ pages/authentication/login.tsx
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { useState } from 'react'
+import TextField from '@material-ui/core/TextField'
+import styles from './../../styles/Login.module.css'
 
 import Figure from './../../components/figure'
 
@@ -9,10 +11,37 @@ export default function Login() {
   return (
     <>
       <Figure listOfStates={[email, password]}/>
+
+      <form className={styles.formLogin}>
+        <TextField
+          label="email"
+          id="outlined-size-small"
+          variant="outlined"
+          size="small"
+          className={styles.inputLogin}
+          style={{marginBottom: '10px'}}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br/>
+
+        <TextField
+          label="password"
+          id="outlined-size-small"
+          variant="outlined"
+          size="small"
+          type="password"
+          className={styles.inputLogin}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </form>
+
       
-      <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
+      {/* <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
       <br/>
-      <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+      <input type="password" value={password} onChange={e => setPassword(e.target.value)}/> */}
     </>
   )
 }
