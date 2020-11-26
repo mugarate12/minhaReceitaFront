978cd372ec260dadeddce75290c41dfee8b9c2c7
mugarate12 <serjumano17@gmail.com>
2020-11-26 11:24:58 -0300

    add logo and changes on home page

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,37 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import Button from '@material-ui/core/Button'
+
 import styles from './../styles/Header.module.css'
 
-export default function Header() {
+type Props = {
+  isHomePage?: boolean;
+}
+
+export default function Header({ isHomePage }: Props) {
+  const router = useRouter()
+  
   return (
     <header className={styles.headerContainer}>
-      <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+      <Link href="/">
+        <a >
+          <img src="/recipe.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </Link>
+
+      {
+        isHomePage ?
+          (<div className={styles.btnContainer}>
+            <Button variant="contained" color="primary" size="medium" style={{marginTop: '15px'}} onClick={() => router.push('/authentication/login')}>
+              Entrar
+            </Button>
+
+            <Button variant="contained" color="secondary" size="medium" style={{marginTop: '15px'}}>
+              Cadastrar
+            </Button>
+          </div>)
+        : (<></>)
+      }
     </header>
   );
 }
