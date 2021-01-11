100ae1fd30417bde1b37ad7997b72d08b8f677ee
mugarate12 <serjumano17@gmail.com>
2021-01-11 11:19:57 -0300

    desktop layout on users/mypage

--- pages/users/mypage.tsx
+++ pages/users/mypage.tsx
@@ -26,8 +26,8 @@ export default function MyPage() {
           <div className={styles.ApresentationImgContainer}>
             <Image
               src='/img/teste.jpg'
-              width={200}
-              height={200}
+              width={270}
+              height={270}
               className={styles.img}
             />
           </div>
@@ -38,25 +38,27 @@ export default function MyPage() {
           </div>
         </div>
 
-        <h5 className={styles.informationTextTitle}>Bio:</h5>
-        <p className={styles.informationTextContent}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum porttitor diam et blandit. Suspendisse potenti. Aenean in fringilla metus, non dignissim lacus. Sed pretium odio ipsum, quis pulvinar mi blandit vitae. Aliquam erat volutpat. In non lacinia leo. Mauris rutrum, turpis non pharetra mattis, ligula nulla iaculis est, in condimentum erat metus hendrerit mauris.</p>
-      
+        <div className={styles.informationContainer}>
+          <h5 className={styles.informationTextTitle}>Bio:</h5>
+          <p className={styles.informationTextContent}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum porttitor diam et blandit. Suspendisse potenti. Aenean in fringilla metus, non dignissim lacus. Sed pretium odio ipsum, quis pulvinar mi blandit vitae. Aliquam erat volutpat. In non lacinia leo. Mauris rutrum, turpis non pharetra mattis, ligula nulla iaculis est, in condimentum erat metus hendrerit mauris.</p>
+        
 
-        <div className={styles.cardsContainer}>
-          <div className={styles.card}>
-            <div className={styles.cardIconContainer}>
-              <ReceiptIcon fontSize='large' color='action'/>
+          <div className={styles.cardsContainer}>
+            <div className={styles.card}>
+              <div className={styles.cardIconContainer}>
+                <ReceiptIcon fontSize='large' color='action'/>
+              </div>
+
+              <p className={styles.cardText}>X receitas</p>
             </div>
 
-            <p className={styles.cardText}>X receitas</p>
-          </div>
+            <div className={styles.card}>
+              <div className={styles.cardIconContainer}>
+                <FastfoodIcon fontSize='large' color='action'/>
+              </div>
 
-          <div className={styles.card}>
-            <div className={styles.cardIconContainer}>
-              <FastfoodIcon fontSize='large' color='action'/>
+              <p className={styles.cardText}>Ver receitas</p>
             </div>
-
-            <p className={styles.cardText}>Ver receitas</p>
           </div>
         </div>
       </div>
